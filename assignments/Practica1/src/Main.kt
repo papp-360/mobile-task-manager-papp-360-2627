497a95e4d1f75f2f652871d657cb8f431823a504
papp-360 <pabloalejandropuente@gmail.com>
2026-09-21 12:41:32 +0200

    He creado los metodos para crear una nueva tarea y para imprimirla por pantalla.

--- assignments/Practica1/src/Main.kt
+++ assignments/Practica1/src/Main.kt
@@ -22,13 +22,70 @@ fun main() {
         )
 
 
+//Metodos del Map
+    fun Task.getId(): Int = this[K_ID] as Int
+    fun Task.getTitle(): String = this[K_TITLE] as String
+    fun Task.getIsDone(): Boolean = this[K_ISDONE] as Boolean
+    fun Task.getDescription(): String = this[K_DESC] as? String
+    fun Task.getDueDate(): String = this[K_DATE] as? String
+    fun Task.getCategory(): String = this[K_CAT] as? String
 
 
+//MutableList de Tasks
+    val tasks = MutableListOf<Task>()
+//funciones
+    var idTask = 0
 
+    //addTask
+    fun addTask(tasks : MutableList<Task>){
 
+        println("\n ---------------------")
+        println("\t\t Add a new task")
+        println("---------------------\n")
 
+        println("Tittle: ")
+        val title = readLine().orEmpty()
+        println("\n Description: ")
+        val description = readLine().orEmpty()
+        println("\n Due date: ")
+        val dueDate = readLine().orEmpty()
+        println("\n Category: ")
+        val category = readLine().orEmpty()
 
+        val task = newTask(idTask, title, false, description, dueDate, category)
+        tasks.add(task)
+    }
 
+    //listTasks
+    fun listTasks(tasks: MutableList<Task>){
         
+        val count = 1;
+        println("\n ---------------------")
+        println("\t\t List of tasks")
+        println("---------------------\n")
+
+        if(Tasks.isEmpty()){
+            println("\n ---------------------")
+            println("\t\t No tasks found")
+            println("---------------------\n")
+        }else{
+            println("\n\ Actual tasks: \n")
+            for(task in tasks){
+                println("\n ---------------------")
+                println("Task #" + count)
+                println("ID: ${task.getId()}, Title: ${task.getTitle()}, Description: ${task.getDescription()}, Due date: ${task.getDueDate()}, Category: ${task.getCategory()}")
+                println("Status: ${if(task.getIsDone()) "Done" else "Not done"}")
+                println("---------------------\n")
+                count++
+            }
+
+        }
+    
+    
+    }
+
+    
+
+
     
 }
