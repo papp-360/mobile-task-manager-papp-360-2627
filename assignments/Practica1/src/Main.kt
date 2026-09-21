b76eedbf0c0d19d4e2d0d53820473120a645b82a
papp-360 <pabloalejandropuente@gmail.com>
2026-09-21 12:03:14 +0200

    Agregué funcion para crear una nueva tarea y un objeto task

--- assignments/Practica1/src/Main.kt
+++ assignments/Practica1/src/Main.kt
@@ -1,14 +1,34 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+const val K_ID = "id"
+const val K_TITLE = "title"
+const val K_ISDONE = "isDone"
+const val K_DESC = "description"
+const val K_DATE = "dueDate"
+const val K_CAT = "category"
+//creamos el objeto task
+typealias Task = Map<String, Any>
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    //funcion para crear una nueva tarea
+    fun newTask(id: Int, title: String, isDone: Boolean, description: String, dueDate: String, category: String): Task = mutableMapOf(
+            K_ID to id,
+            K_TITLE to title,
+            K_ISDONE to isDone,
+            K_DESC to description,
+            K_DATE to dueDate,
+            K_CAT to category
+        )
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
+
+
+
+
+
+
+
+        
+    
 }
